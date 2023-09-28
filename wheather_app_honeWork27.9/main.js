2e69a5565eed85180f5b3b322d161df0b8efbf97
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-09-28 20:53:01 +0300

    comit

--- wheather_app_honeWork27.9/main.js
+++ wheather_app_honeWork27.9/main.js
@@ -9,12 +9,15 @@ const fetchWheather=(City)=>
            data.list.forEach((temp,index) => {
             if(index%8==0){
                 mainDiv.innerHTML+=` 
-                <h2><div style='color:purple;'>the date: </div>${temp.dt_txt.substring(0,10)}</h2>`
-            }
-            mainDiv.innerHTML+=`
-                    <h3> <div style='color:blue;'>the hour:</div>${temp.dt_txt.substring(10,temp.dt_txt.length)}</h3>
+                <h2><div style='color:purple;'>the date: </div>${temp.dt_txt.substring(0,10)}</h2>
+                <h3> <div style='color:blue;'>the hour:</div>${temp.dt_txt.substring(10,temp.dt_txt.length)}</h3>
                     <p><b style='color:red;'>the temp is:</b> ${temp.main.temp}</p>
                     <img src='https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png'>`
+            }
+            // mainDiv.innerHTML+=`
+            //         <h3> <div style='color:blue;'>the hour:</div>${temp.dt_txt.substring(10,temp.dt_txt.length)}</h3>
+            //         <p><b style='color:red;'>the temp is:</b> ${temp.main.temp}</p>
+            //         <img src='https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png'>`
            });
             
         })
@@ -26,3 +29,4 @@ const fetchWheather=(City)=>
 
     //${new Date(data.list[0].dt_txt).getDate()}/${new Date(data.list[0].dt_txt).getMonth()+1}/${new Date(data.list[0].dt_txt).getFullYear()}
     //${new Date(data.list[0].dt_txt).getHours()}:${new Date(data.list[0].dt_txt).getMinutes()}
+    
