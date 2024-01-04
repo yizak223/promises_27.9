be51dda4eb431b17bea441ea4b41f58bb594e9ca
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-01-04 17:53:27 +0200

    commit

--- local_storage_json_28.9/main.js
+++ local_storage_json_28.9/main.js
@@ -1,21 +1,21 @@
-const BOOKS_ARRAY=[
-    {
-        Title:'harrry potter',
-        Pages:600,
-        Author:'J.K.R'
-    },
-    {
-        Title:'minehert hazman',
-        Pages:400,
-        Author:'yizak alaluf'
-    },
-    {
-        Title:'lord of ring',
-        Pages:500,
-        Author:'Ben kilinski'
-    },
-]
-console.log(BOOKS_ARRAY);
+// const BOOKS_ARRAY=[
+//     {
+//         Title:'harrry potter',
+//         Pages:600,
+//         Author:'J.K.R'
+//     },
+//     {
+//         Title:'minehert hazman',
+//         Pages:400,
+//         Author:'yizak alaluf'
+//     },
+//     {
+//         Title:'lord of ring',
+//         Pages:500,
+//         Author:'Ben kilinski'
+//     },
+// ]
+// console.log(BOOKS_ARRAY);
 // function Book(title,pages,author) {
 //     this.Title=title;
 //     this.Pages=pages;
@@ -29,16 +29,18 @@ const authorBook=document.querySelector('#authorBook')
 const NEW_BOOK_ARRAY=[]
 btnSubmit.addEventListener('click',(e)=>{
     e.preventDefault();
-    BOOKS_ARRAY.push({Title:nameBook.value,Pages:pagesBook.value,Author:authorBook.value})
+    // BOOKS_ARRAY.push({Title:nameBook.value,Pages:pagesBook.value,Author:authorBook.value})
     NEW_BOOK_ARRAY.push({Title:nameBook.value,Pages:pagesBook.value,Author:authorBook.value})
-    console.log(BOOKS_ARRAY);
+    // console.log(BOOKS_ARRAY);
     localStorage.setItem('BOOKS',JSON.stringify(NEW_BOOK_ARRAY))
-    localStorage.setItem('BOOKS',JSON.stringify(BOOKS_ARRAY))
+    // localStorage.setItem('BOOKS',JSON.stringify(BOOKS_ARRAY))
+    
     
 })
 
 console.log(localStorage.getItem('BOOKS'));
 
 console.log(JSON.parse(localStorage.getItem('BOOKS')));
-console.log(BOOKS_ARRAY);
+// console.log(BOOKS_ARRAY);
 console.log(NEW_BOOK_ARRAY);
+// localStorage.clear();
